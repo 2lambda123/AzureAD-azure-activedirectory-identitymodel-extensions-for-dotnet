elarrieux/azure-activedirectory-identitymodel-extensions-for-dotnet

First cut async validators

test/Microsoft.IdentityModel.Tokens.Tests/TokenValidationParametersTests.cs
@@ -291,7 +291,7 @@ namespace Microsoft.IdentityModel.Tokens.Tests
             validationParameters.IssuerSigningKeyValidator = ValidationDelegates.IssuerSigningKeyValidator;
             validationParameters.IssuerSigningKeyValidatorUsingConfiguration = ValidationDelegates.IssuerSigningKeyValidatorUsingConfiguration;
             validationParameters.IssuerValidator = ValidationDelegates.IssuerValidatorEcho;
-            validationParameters.IssuerValidatorAsync = ValidationDelegates.IssuerValidatorAsync;
+            validationParameters.IssuerValidatorInternalAsync = ValidationDelegates.IssuerValidatorInternalAsync;
             validationParameters.IssuerValidatorUsingConfiguration = ValidationDelegates.IssuerValidatorUsingConfigEcho;
             validationParameters.LifetimeValidator = ValidationDelegates.LifetimeValidatorReturnsTrue;
             validationParameters.NameClaimTypeRetriever = ValidationDelegates.NameClaimTypeRetriever;

2lambda123/AzureAD-azure-activedirectory-identitymodel-extensions-for-dotnet

test/Microsoft.IdentityModel.Tokens.Tests/TokenValidationParametersTests.cs
@@ -291,7 +291,7 @@ namespace Microsoft.IdentityModel.Tokens.Tests
             validationParameters.IssuerSigningKeyValidator = ValidationDelegates.IssuerSigningKeyValidator;
             validationParameters.IssuerSigningKeyValidatorUsingConfiguration = ValidationDelegates.IssuerSigningKeyValidatorUsingConfiguration;
             validationParameters.IssuerValidator = ValidationDelegates.IssuerValidatorEcho;
-            validationParameters.IssuerValidatorAsync = ValidationDelegates.IssuerValidatorAsync;
+            validationParameters.IssuerValidatorAsync = ValidationDelegates.IssuerValidatorInternalAsync;
             validationParameters.IssuerValidatorUsingConfiguration = ValidationDelegates.IssuerValidatorUsingConfigEcho;
             validationParameters.LifetimeValidator = ValidationDelegates.LifetimeValidatorReturnsTrue;
             validationParameters.NameClaimTypeRetriever = ValidationDelegates.NameClaimTypeRetriever;